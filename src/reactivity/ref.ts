72f9978920556615201f10090be35e72a3766e8c
sunjiang <sunjiang0018@gmail.com>
2022-07-05 10:24:16 +0800

    实现isRef、unRef、proxyRefs功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -4,6 +4,7 @@ import { reactive } from './reactive';
 
 class RefImpl {
   private _value: any;
+  public __v_isRef = true;
 
   public dep = new Set();
   private _rawValue: any;
@@ -29,10 +30,38 @@ class RefImpl {
   }
 }
 
-function convert(value: any) {
-  return isObject(value) ? reactive(value) : value;
+export function isRef(value: any) {
+  return !!value.__v_isRef;
+}
+
+export function unRef(raw: any) {
+  return isRef(raw) ? raw.value : raw;
 }
 
 export function ref(value: any) {
   return new RefImpl(value);
+}
+
+export function proxyRefs(objectWithRefs: any) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      const value = Reflect.get(target, key);
+      return unRef(value);
+    },
+    set(target, key, value){
+
+      const oldValue = Reflect.get(target, key)
+
+      if(isRef(oldValue) && !isRef(value)){
+        oldValue.value = value
+        return true
+      }
+      return Reflect.set(target, key, value)
+
+    }
+  });
+}
+
+function convert(value: any) {
+  return isObject(value) ? reactive(value) : value;
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,49 +1,88 @@
-import { effect } from "../effect"
-import { ref } from "../ref"
+import { effect } from '../effect';
+import { reactive } from '../reactive';
+import { isRef, proxyRefs, ref, unRef } from '../ref';
 
-describe('ref',()=>{
-  it('happy path',()=>{
-    const obj = ref(0)
+describe('ref', () => {
+  it('happy path', () => {
+    const obj = ref(0);
 
-    expect(obj.value).toBe(0)
-  })
+    expect(obj.value).toBe(0);
+  });
 
-  it('should be reactive',()=>{
-
+  it('should be reactive', () => {
     let dumy;
     let calls = 0;
     const obj = ref(1);
 
-    effect(()=>{
+    effect(() => {
       calls++;
       dumy = obj.value;
-    })
-
+    });
 
     expect(calls).toBe(1);
     expect(dumy).toBe(1);
 
     obj.value = 2;
-    expect(calls).toBe(2)
-    expect(dumy).toBe(2)
+    expect(calls).toBe(2);
+    expect(dumy).toBe(2);
 
     obj.value = 2;
-    expect(calls).toBe(2)
-    expect(dumy).toBe(2)
-  })
-  
-  it('should make nested properties reactive',()=>{
+    expect(calls).toBe(2);
+    expect(dumy).toBe(2);
+  });
+
+  it('should make nested properties reactive', () => {
     const obj = ref({
-      count:1
-    })
+      count: 1,
+    });
 
     let dumy;
-    effect(()=>{
-      dumy = obj.value.count
-    })
+    effect(() => {
+      dumy = obj.value.count;
+    });
 
-    expect(dumy).toBe(1)
-    obj.value.count = 2
-    expect(dumy).toBe(2)
-  })
-})
+    expect(dumy).toBe(1);
+    obj.value.count = 2;
+    expect(dumy).toBe(2);
+  });
+
+  it('isRef', () => {
+    const a = ref(1);
+
+    const obj = reactive({ count: 1 });
+
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(obj)).toBe(false);
+  });
+
+  it('unRef', () => {
+    const a = ref(1);
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+  });
+
+  it('proxyRefs', () => {
+    const user = {
+      age: ref(10),
+      name: 'vue-study',
+    };
+
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe('vue-study');
+
+    proxyUser.age = 20
+
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
+    
+    proxyUser.age = ref(10)
+
+    expect(proxyUser.age).toBe(10)
+    expect(user.age.value).toBe(10)
+
+
+  });
+});
